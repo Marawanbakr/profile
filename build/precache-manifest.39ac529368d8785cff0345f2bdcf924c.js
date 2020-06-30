053e62c41d8d2c7ba421ba552e84c36b60a6ac6e
marwan <marwanabuabk0o0@gmail.com>
2020-06-30 18:44:55 +0200

    end

---
+++ build/precache-manifest.39ac529368d8785cff0345f2bdcf924c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7acfd1e27e787869a93fe5a130f17a21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa950d29583b323e4841",
+    "url": "/static/css/main.3d721367.chunk.css"
+  },
+  {
+    "revision": "930d8b88d0d225aff42d",
+    "url": "/static/js/2.bd7ac11b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bd7ac11b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa950d29583b323e4841",
+    "url": "/static/js/main.0a8dca6f.chunk.js"
+  },
+  {
+    "revision": "b2f1d241e92159ae220a",
+    "url": "/static/js/runtime-main.8fc600bd.js"
+  }
+]);
